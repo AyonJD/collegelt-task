94e343efe6a5bfed6ffee464cfcdac4260ca48fd
Ayon <ayonjodder177@gmail.com>
2022-06-07 14:14:43 +0600

    Table added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="overflow-x-auto container mx-auto">
+      <table className="table w-full">
+        {/* <!-- head --> */}
+        <thead>
+          <tr>
+            <th>Serial</th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="hover">
+            <th>1</th>
+            <td>Cy Ganderton</td>
+            <td>Quality</td>
+            <td>Blue</td>
+            <td>Blue</td>
+            <td>Blue</td>
+          </tr>
+          
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
